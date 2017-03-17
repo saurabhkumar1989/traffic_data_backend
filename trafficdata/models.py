f475a88c17209e30ac2a53a243e9ae4937f87b64
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-03-17 17:29:46 -0400

    sum module problem solved

---
+++ trafficdata/models.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+# Create your models here.
+class Tweets(models.Model):
+	tweet_id = models.BigIntegerField(primary_key = True)
+	tweet_text = models.CharField(max_length = 200,null=True)
+	user_id = models.ForeignKey(
+		'User',
+		)
+	query_id = models.ForeignKey(
+		'Query'
+		)
+	tweet_date = models.DateTimeField(auto_now_add=True,null=True)
+	traffic_info = models.CharField(max_length=10,null=True)#  classifier output
+	location = models.CharField(max_length=100,null=True)
+class  User(models.Model):
+	#user_id= models.AutoField(primary_key=True) not require, django will autoatically do that 
+	user_Twitter_id = models.CharField(max_length = 50,null=True)
+	screen_name = models.CharField(max_length = 50,null=True)
+	followers_count = models.IntegerField(null=True)
+	friends_count = models.IntegerField(null=True)
+	time_zone = models.CharField(max_length = 100,null=True)
+class Query(models.Model):
+	query = models.ForeignKey(
+		'QueryType'
+		)
+	city = models.ForeignKey(
+		'City'
+		)
+	key_word = models.ForeignKey(
+		'KeyWords'
+		)
+
+class City(models.Model):
+	#city_id =  models.AutoField(primary_key=True) not require, django will autoatically do that 
+	location = models.CharField(max_length=50,null=False)# city longitatude and latitude with radius
+	city_name = models.CharField(max_length=50,null=False)# name of the city
+
+class QueryType(models.Model):
+	#query_id = models.IntegerField(primary_key=True)#not require, django will autoatically do that 
+	query_type = models.CharField(max_length=30)## only two type for now - 1 User time line and 2 Key word + location based
+
+class KeyWords(models.Model):
+	#key_word_id = models.AutoField(primary_key=True)#not require, django will autoatically do that 
+	key_word = models.CharField(max_length=20)# key word for the city like rain,traffic water and so on
---
+++ trafficdata/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import get_object_or_404, render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from .models import Tweets
+from django.template import loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+PAGE_SIZE = 10
+
+def index(request):
+	data_list = Tweets.objects.all().order_by('-tweet_date')
+	paginator = Paginator(data_list,PAGE_SIZE)
+	page = request.GET.get('page')
+	try:
+		data = paginator.page(page)
+	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+		data = paginator.page(1)
+	except EmptyPage:
+       # If page is out of range (e.g. 9999), deliver last page of results.
+		data = paginator.page(paginator.num_pages)
+	context = {
+		'data_list': data,
+	}
+	return render(request, 'trafficdata/traffic_data_frontend/index.html', context)
+	
+
+def detail(request,tweet_id):
+	data = []
+	pass
+def about(request):
+	pass
