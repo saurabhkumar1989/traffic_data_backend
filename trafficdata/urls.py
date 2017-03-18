883b5a6dffef24be885d8d7d87227c053f0799cf
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-03-18 11:44:16 -0400

    search functionality

--- trafficdata/urls.py
+++ trafficdata/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search-result/$', views.search, name='search'),
     url(r'^(?P<id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^about/$', views.about, name='about'),
 ]
--- trafficdata/views.py
+++ trafficdata/views.py
@@ -5,12 +5,13 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-from .models import Tweets
+from .models import Tweets,User
 from django.template import loader
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 PAGE_SIZE = 10
 
 def index(request):
+	users_list = User.objects.all()
 	data_list = Tweets.objects.all().order_by('-tweet_date')
 	paginator = Paginator(data_list,PAGE_SIZE)
 	page = request.GET.get('page')
@@ -22,8 +23,10 @@ def index(request):
 	except EmptyPage:
        # If page is out of range (e.g. 9999), deliver last page of results.
 		data = paginator.page(paginator.num_pages)
+	#print(users)
 	context = {
-		'data_list': data,
+		'data': data,
+		'users_list': users_list,
 	}
 	return render(request, 'trafficdata/traffic_data_frontend/index.html', context)
 	
@@ -35,5 +38,18 @@ def detail(request,id):
 		'data': data,
 	}
 	return render(request, 'trafficdata/traffic_data_frontend/detail.html', context)
+def search(request):
+	users_list = User.objects.all()
+	if request.method == "GET":
+		userQuery = request.GET.get('userQuery')
+		userOption = request.GET.get('user')
+		user_id = User.objects.get(screen_name = userOption)
+		data = Tweets.objects.filter(user_id = user_id )
+		context = {
+		'data': data,
+		'users_list': users_list,
+		}
+	return render(request, 'trafficdata/traffic_data_frontend/index.html',context)
+
 def about(request):
 	pass
