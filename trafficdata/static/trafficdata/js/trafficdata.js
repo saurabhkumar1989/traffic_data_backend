b8e2b6fa8075d15cb81d2fd110a84d6c6a5fd114
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-04-04 12:33:16 -0400

    minor changes

--- trafficdata/static/trafficdata/js/trafficdata.js
+++ trafficdata/static/trafficdata/js/trafficdata.js
@@ -1,1 +1,8 @@
-$('a').tooltip();
+$(document).ready(function(){
+	$('a').tooltip();//activate tool tooltip
+    $('#toggleView').click(function(){
+
+        console.log("dd");
+        //console.log(this.text())
+    });
+});
